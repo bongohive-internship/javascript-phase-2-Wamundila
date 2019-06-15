37a6d73ab78a0de7ada7cfad99e4f81d8d0f56b9
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-15 23:42:15 +0200

    print date try

--- print-date.js
+++ print-date.js
@@ -1,2 +1,20 @@
 // add a printDate function in here
 // eg: console.log("Hello World")
+
+function printDateAndTime()
+{
+	// var year = new getFullYear();
+	// var y = year.getFullYear();
+
+	var month = new getMonth();
+	var m = month.getMonth();
+
+	var day = new getDate();
+	var d = day.getDate();
+
+	
+	console.log(d+"//"+m+"//" + y);
+	console.log("what up");
+}
+
+printDateAndTime();
