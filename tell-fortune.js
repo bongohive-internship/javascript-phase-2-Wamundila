608e881f671e10fa042b53cfdd66fd6e7cf7cf3f
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-16 00:56:16 +0200

    Add tellFortune function to tell-fortune.js

--- tell-fortune.js
+++ tell-fortune.js
@@ -1,1 +1,12 @@
 // tellFortune function should be here
+function tellFortune(numberOfChildren,partnerName,jobTitle,geoLocation)
+{
+	//assigning shorter names to the variables
+	var nOC = numberOfChildren;
+	var pN = partnerName;
+	var jT = jobTitle;
+	var gL = geoLocation;
+	//Print a fortune on the console
+	console.log("You will be "+jT+" in "+gL+", and married to "+pN+" with "+nOC+" kids.");
+
+}
