56273c1b6f8c47c74b03ef0b78771656b61a8e80
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-16 01:12:07 +0200

    Add console logs to tell-fortune

--- tell-fortune.js
+++ tell-fortune.js
@@ -10,3 +10,10 @@ function tellFortune(numberOfChildren,partnerName,jobTitle,geoLocation)
 	console.log("You will be "+jT+" in "+gL+", and married to "+pN+" with "+nOC+" kids.");
 
 }
+
+var fortune1 = tellFortune("2","Rachael Zulu","Soldier","Kitwe");
+console.log(fortune1);
+var fortune1 = tellFortune("3","Martha Mulenga","Teacher","Lundazi");
+console.log(fortune1);
+var fortune1 = tellFortune("7","Christina Muyunda","Programmer","Lusaka");
+console.log(fortune1);
