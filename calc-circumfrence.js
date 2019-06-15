f2ca6e64bff13955c15c5defa8b648f0accbd868
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-16 01:21:51 +0200

    Add circumference calculator to calc-circumfrence.js

--- calc-circumfrence.js
+++ calc-circumfrence.js
@@ -1,1 +1,8 @@
 // calcCircumfrence function should be here
+function circumference(radius)
+{
+	//formula for circumference is c = 2*pi*radius
+	var C = 2*3.142*radius;
+	console.log("The circumference is "+C);
+
+}
